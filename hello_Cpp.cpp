7fbc3ff72eeaff3d2d0af3e17d8bfcbfb2e05fd3
Spring <3882714715@qq.com>
2026-05-29 10:05:02 +0800

    update hello_Cpp file

---
+++ hello_Cpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>  //the C++ language struct
+using namespace std;
+
+int main()
+{
+	cout << "hello C++" << endl;  //print the sentence "hello C++"
+
+	system("pause");
+
+	return 0;
+}
